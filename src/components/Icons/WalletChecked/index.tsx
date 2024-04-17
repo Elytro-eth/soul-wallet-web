9925ef1ca1f4cf3a669ab2af5c699286ac7c1085
Terence Ge <terencegehui@yahoo.com>
2024-04-17 20:15:17 +0800

    chore: modify recover

---
+++ src/components/Icons/WalletChecked/index.tsx
@@ -0,0 +1,9 @@
+const WalletChecked = () => {
+  return (
+    <svg width="12" height="11" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fillRule="evenodd" clipRule="evenodd" d="M3.71922 8.44793L0.929804 5.10064L0 6.2164L3.71922 10.6795L11.689 1.11576L10.7592 0L3.71922 8.44793Z" fill="#0CB700" />
+    </svg>
+  );
+};
+
+export default WalletChecked;
