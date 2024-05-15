dad4d6c33a30ff87a68cc92d03056c3a9a3416fe
rocker <exrobbie@gmail.com>
2024-05-15 22:33:31 +0800

    feat: optimized logic

--- src/components/Pooling.tsx
+++ src/components/Pooling.tsx
@@ -7,15 +7,27 @@ import { useAddressStore } from '@/store/address';
 import { useHistoryStore } from '@/store/history';
 import { useBalanceStore } from '@/store/balance';
 import useConfig from '@/hooks/useConfig';
+import BN from 'bignumber.js';
+import { toFixed, toShortAddress } from '@/lib/tools';
 import { useChainStore } from '@/store/chain';
 import useWalletContext from '@/context/hooks/useWalletContext';
+import useWallet from '@/hooks/useWallet';
 export default function Pooling() {
-  const { ethersProvider } = useWalletContext();
-  const { selectedAddress } = useAddressStore();
+  const { ethersProvider, checkActivated } = useWalletContext();
+  const { selectedAddress, setActivateFee } = useAddressStore();
   const { fetchHistory } = useHistoryStore();
   const { selectedChainItem } = useConfig();
+  const { getActivateFee } = useWallet();
   const { selectedChainId } = useChainStore();
   const { fetchTokenBalance } = useBalanceStore();
+
+  const checkActivateFee = async () => {
+    if (!(await checkActivated())) {
+      const fee = await getActivateFee();
+      // add 0.001 more
+      setActivateFee(toFixed(BN(fee).plus(0.001).toString(), 6));
+    }
+  };
 
   useEffect(() => {
     const { chainIdHex, paymasterTokens } = selectedChainItem;
@@ -26,10 +38,12 @@ export default function Pooling() {
 
     fetchTokenBalance(selectedAddress, chainIdHex, paymasterTokens);
     fetchHistory(selectedAddress, [selectedChainId], ethersProvider);
+    checkActivateFee();
 
     const interval = setInterval(() => {
       fetchTokenBalance(selectedAddress, chainIdHex, paymasterTokens);
       fetchHistory(selectedAddress, [selectedChainId], ethersProvider);
+      checkActivateFee();
     }, 8000);
 
     return () => {
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,15 +4,20 @@ import IconAssets from '@/assets/icons/sidebar/assets.svg';
 import IconAssetsActive from '@/assets/icons/sidebar/assets-active.svg';
 import IconActivity from '@/assets/icons/sidebar/activity.svg';
 import IconActivityActive from '@/assets/icons/sidebar/activity-active.svg';
-import IconDapps from '@/assets/icons/sidebar/dapps.svg';
-import IconDappsActive from '@/assets/icons/sidebar/dapps-active.svg';
+// import IconDapps from '@/assets/icons/sidebar/dapps.svg';
+// import IconDappsActive from '@/assets/icons/sidebar/dapps-active.svg';
 import IconSettings from '@/assets/icons/sidebar/settings.svg';
 import IconSettingsActive from '@/assets/icons/sidebar/settings-active.svg';
+import IconEmailGmail from '@/assets/emails/gmail.svg';
+import IconEmailOutlook from '@/assets/emails/outlook.svg';
+import IconEmailYahoo from '@/assets/emails/yahoo.svg';
+import IconEmailIcloud from '@/assets/emails/icloud.svg';
+import IconEmailProton from '@/assets/emails/proton.svg';
 
 export const sidebarLinks = [
   {
     title: 'Dashboard',
-    href: "/dashboard",
+    href: '/dashboard',
     icon: IconDashboard,
     iconActive: IconDashboardActive,
     isComing: false,
@@ -20,7 +25,7 @@ export const sidebarLinks = [
   },
   {
     title: 'Assets',
-    href: "/asset",
+    href: '/asset',
     icon: IconAssets,
     iconActive: IconAssetsActive,
     isComing: false,
@@ -28,7 +33,7 @@ export const sidebarLinks = [
   },
   {
     title: 'Activity',
-    href: "/activity",
+    href: '/activity',
     icon: IconActivity,
     iconActive: IconActivityActive,
     isComing: false,
@@ -43,7 +48,7 @@ export const sidebarLinks = [
   // },
   {
     title: 'Settings',
-    href: "/security",
+    href: '/security',
     icon: IconSettings,
     iconActive: IconSettingsActive,
     isComing: false,
@@ -51,16 +56,41 @@ export const sidebarLinks = [
   },
 ];
 
-
-export const passkeyTooltipText = "A passkey is a FIDO credential stored on your computer or phone, and it is used to unlock your online accounts. The passkey makes signing in more secure."
+export const passkeyTooltipText =
+  'A passkey is a FIDO credential stored on your computer or phone, and it is used to unlock your online accounts. The passkey makes signing in more secure.';
 
 export const forbiddenEmailProviders = ['qq.com', '163.com', '126.com'];
 
-export const validEmailDomains = [
-  'gmail.com',
-  'outlook.com',
-  'yahoo.com',
-  'icloud.com',
-  'proton.me',
-  'protonmail.com'
-]
+// export const validEmailDomains = ['gmail.com', 'outlook.com', 'yahoo.com', 'icloud.com', 'proton.me', 'protonmail.com'];
+
+export const validEmailProviders = [
+  {
+    icon: IconEmailGmail,
+    title: 'Gmail',
+    domain: 'gmail.com',
+  },
+  {
+    icon: IconEmailOutlook,
+    title: 'Outlook',
+    domain: 'outlook.com',
+  },
+  {
+    icon: IconEmailYahoo,
+    title: 'Yahoo mail',
+    domain: 'yahoo.com',
+  },
+  {
+    icon: IconEmailIcloud,
+    title: 'Icloud',
+    domain: 'icloud.com',
+  },
+  {
+    icon: IconEmailProton,
+    title: 'Pronton',
+    domain: 'proton.me',
+  },
+];
+
+const extraEmailProviders = ['protonmail.com'];
+
+export const validEmailDomains = [validEmailProviders.map((provider) => provider.domain), ...extraEmailProviders];
