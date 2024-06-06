9008cddf0ac64696c3e57da70bc1d2aaa5b80ece
Terence Ge <terencegehui@yahoo.com>
2024-06-06 14:16:37 +0800

    chore: update

--- src/pages/send/Review/index.tsx
+++ src/pages/send/Review/index.tsx
@@ -1,188 +1,208 @@
-import { Box, Image } from '@chakra-ui/react';
-import Button from '@/components/mobile/Button';
-import Header from '@/components/mobile/Header';
-// import CompletedIcon from '@/components/Icons/mobile/Completed';
-import { Link } from 'react-router-dom';
-import LoadingIcon from '@/assets/mobile/loading.gif';
-import CompletedIcon from '@/assets/mobile/completed.gif';
-import CompletedStaticIcon from '@/assets/mobile/completed_static.svg';
-import { useEffect, useRef, useState } from 'react';
-import useWallet from '@/hooks/useWallet';
-import useWalletContext from '@/context/hooks/useWalletContext';
+import { useEffect, useRef, useState, useCallback, Fragment } from 'react'
+import { Box, Input, Image, Modal, ModalOverlay, ModalContent, ModalCloseButton, ModalBody, useDisclosure, Link } from '@chakra-ui/react';
+import Button from '@/components/mobile/Button'
+import Header from '@/components/mobile/Header'
+import QuestionIcon from '@/components/Icons/Question'
+import SendingIcon from '@/components/Icons/mobile/Sending'
+import SentIcon from '@/components/Icons/mobile/Sent'
+import { useBalanceStore } from '@/store/balance';
+import BN from 'bignumber.js'
+import OpIcon from '@/assets/mobile/op.png'
+import USDCIcon from '@/assets/mobile/usdc.png'
+import { toFixed } from '@/lib/tools';
+import { isAddress } from 'ethers';
 
-export default function Review({ onPrev, withdrawAmount, sendTo, isModal }: any) {
-  const { closeModal } = useWalletContext()
-  const { getWithdrawOp, signAndSend, getTransferEthOp } = useWallet();
-  const executingRef = useRef(false);
-  const userOpRef = useRef();
-  const isCompletedRef = useRef(false);
-  const isTransferingRef = useRef(false);
-
-  const [animated, setAnimated] = useState(false)
-  const [completed, setCompleted] = useState(false)
-
-  const prepareAction = async () => {
-    try {
-      const _userOp = await getTransferEthOp(withdrawAmount, sendTo);
-      userOpRef.current = _userOp;
-    } catch (e) {
-      // user may reach limit of gas sponsor
-      executingRef.current = false;
-      isTransferingRef.current = false;
-      closeModal();
-    }
-  };
-
-  useEffect(() => {
-    prepareAction();
-    const interval = setInterval(() => {
-      if (isTransferingRef.current || isCompletedRef.current) {
-        return;
-      }
-      prepareAction();
-    }, 15000);
-    return () => clearInterval(interval);
-  }, []);
-
-  const onWithdraw = async (skipExecutingCheck = false) => {
-    isTransferingRef.current = true;
-    if (executingRef.current && !skipExecutingCheck) {
-      return;
-    }
-    executingRef.current = true;
-    isTransferingRef.current = true;
-    if (userOpRef.current) {
-      try {
-        await signAndSend(userOpRef.current);
-        isTransferingRef.current = false;
-        isCompletedRef.current = true;
-        setAnimated(false)
-        setCompleted(true)
-        console.log('setAnimated false')
-
-        setTimeout(() => {
-          setAnimated(true)
-          console.log('setAnimated true')
-        }, 1300)
-      } catch (e) {
-        // setExecuting(false);
-        executingRef.current = false;
-        isTransferingRef.current = false;
-        isCompletedRef.current = false;
-      }
-    } else {
-      executingRef.current = false;
-      setTimeout(() => {
-        onWithdraw(true);
-      }, 1000);
-    }
-  };
+export default function Review({ isModal, onPrev }: any) {
+  const [withdrawAmount, setWithdrawAmount] = useState<any>('');
+  const [sendTo, setSendTo] = useState('');
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isSending, setIsSending] = useState(false);
+  const [isSent, setIsSent] = useState(false);
+  const innerHeight = window.innerHeight
+  const marginHeight = innerHeight - 468
 
   return (
-    <Box width="100%" maxWidth="100vw" height="100%">
-      <Header title="Review" showBackButton onBack={onPrev} />
+    <Box width="100%" height={innerHeight} overflowY="scroll">
+      <Header
+        title=""
+        showBackButton={!isModal}
+        onBack={onPrev}
+      />
       <Box padding="30px" minHeight={isModal ? 'calc(100vh - 118px)' : 'calc(100vh - 62px)'}>
-        {isTransferingRef.current && (
+        {(isSent) && (
+          <Box fontSize="32px" fontWeight="700" display="flex" alignItems="center">
+            <Box marginRight="12px"><SentIcon /></Box>
+            Completed
+          </Box>
+        )}
+        {(isSending && !isSent) && (
+          <Box fontSize="32px" fontWeight="700" display="flex" alignItems="center">
+            <Box marginRight="12px"><SendingIcon /></Box>
+            Sending
+          </Box>
+        )}
+        {(!isSending && !isSent) && (
+          <Box fontSize="32px" fontWeight="700">
+            Review
+          </Box>
+        )}
+        <Box marginTop="60px">
           <Box
-            fontSize="32px"
+            fontSize="14px"
             fontWeight="700"
-            lineHeight="42px"
-            textAlign="center"
-            height="200px"
+            opacity="0.4"
+          >
+            Send
+          </Box>
+          <Box marginTop="8px" display="flex" alignItems="center">
+            <Box marginRight="8px"><Image w="32px" h="32px" src={USDCIcon} /></Box>
+            <Box fontSize="24px" fontWeight="600">10.1111 ETH</Box>
+          </Box>
+
+          <Box
+            padding="10px 0"
             display="flex"
             alignItems="center"
-            justifyContent="flex-end"
-            flexDirection="column"
+            position="relative"
           >
-            <Box marginBottom="6px" marginTop="20px">
-              <Image width="80px" height="80px" src={LoadingIcon} />
-            </Box>
-            <Box>Transfer in progress</Box>
           </Box>
-        )}
-        {isCompletedRef.current && completed && (
+        </Box>
+        <Box marginTop="60px">
           <Box
-            fontSize="32px"
+            fontSize="14px"
             fontWeight="700"
-            lineHeight="42px"
-            textAlign="center"
-            height="200px"
+            opacity="0.4"
+          >
+            To
+          </Box>
+          <Box
+            marginTop="8px"
+            alignItems="center"
+            width="100%"
+            display="inline-block"
+          >
+            <Box as="span" fontSize="24px" fontWeight="600">0x8d34</Box>
+            <Box as="span" fontSize="24px" fontWeight="600" color="rgba(0, 0, 0, 0.4)">947d8cba2abd7e8d5b788c8a3674325c93d1</Box>
+            <Box as="span" fontSize="24px" fontWeight="600">5c93d1</Box>
+          </Box>
+
+          <Box
+            padding="10px 0"
             display="flex"
             alignItems="center"
-            justifyContent="flex-end"
-            flexDirection="column"
+            position="relative"
           >
-            <Box width="100px" height="100px" position="relative" marginBottom="6px">
-              {!animated && <Image position="absolute" left="0" top="0" width="100px" height="100px" src={CompletedIcon} />}
-              <Image position="absolute" left="13px" top="13px" width="74px" height="74px" src={CompletedStaticIcon} />
-            </Box>
-            <Box>Transfer completed</Box>
           </Box>
-        )}
-        {!isCompletedRef.current && !isTransferingRef.current && (
-          <Box
-            fontSize="32px"
-            fontWeight="700"
-            lineHeight="42px"
-            textAlign="center"
-            height="160px"
-            display="flex"
-            alignItems="flex-end"
-            justifyContent="center"
-          >
-            Please confirm the detail below
-          </Box>
-        )}
+        </Box>
         <Box
-          width="100%"
-          borderRadius="24px"
-          background="white"
-          padding="24px"
+          fontSize="14px"
+          fontWeight="700"
+          opacity="0.4"
           marginTop="40px"
-          display="flex"
-          flexDirection="column"
-          boxShadow="0px 4px 60px 0px rgba(44, 53, 131, 0.08)"
         >
-          <Box paddingTop="13px" paddingBottom="27px" borderBottom="1px solid rgba(73, 126, 230, 0.2)">
-            <Box fontSize="16px" color="#818181" marginBottom="16px">
-              Transfer
+          Network
+        </Box>
+        <Box onClick={onOpen} marginTop="8px" display="flex" alignItems="center">
+          <Box marginRight="8px"><Image w="32px" h="32px" src={OpIcon} /></Box>
+          <Box fontSize="20px" fontWeight="600">Optimism</Box>
+          <Box width="40px" height="40px" display="flex" alignItems="center" justifyContent="center"><QuestionIcon /></Box>
+        </Box>
+        <Box
+          fontSize="14px"
+          fontWeight="700"
+          opacity="0.4"
+          marginTop="40px"
+        >
+          Fee
+        </Box>
+        <Box marginTop="8px" display="flex" alignItems="center">
+          <Box fontSize="20px" fontWeight="600">$0</Box>
+        </Box>
+        {isSent && (
+          <Box
+            marginTop="40px"
+            width="100%"
+          >
+            <Box width="100%" marginBottom="20px">
+              <Button width="calc(100%)" disabled={false} size="xl" type="blue">Done</Button>
             </Box>
-            <Box fontSize="32px" lineHeight={'1'} fontWeight="700">
-              {withdrawAmount} USDC
+            <Box width="100%">
+              <Button width="calc(100%)" disabled={false} size="xl" type="white">Copy transaction link</Button>
             </Box>
           </Box>
-          <Box paddingTop="26px" paddingBottom="12px">
-            <Box fontSize="16px" color="#818181" marginBottom="16px">
-              To
+        )}
+        {!isSent && (
+          <Box
+            marginTop="40px"
+            width="100%"
+            display="flex"
+          >
+            <Box width="50%" paddingRight="7px">
+              <Button width="calc(100% - 7px)" disabled={false} size="xl" type="white" onClick={onPrev} color="black">Back</Button>
             </Box>
-            <Box fontSize="18px" fontWeight="700">
-              {sendTo}
+            <Box width="50%" paddingLeft="7px">
+              <Button width="calc(100% - 7px)" disabled={false} size="xl" type="blue" onClick={() => { setIsSending(true); setTimeout(() => { setIsSent(true) }, 2000)}}>Continue</Button>
             </Box>
           </Box>
-        </Box>
-
-        <Box marginTop="40px" width="100%">
-          {isTransferingRef.current && (
-            <Button size="xl" type="black" width="100%" isDisabled>
-              Transferring
-            </Button>
-          )}
-
-          {!isTransferingRef.current && !isCompletedRef.current && (
-            <Button size="xl" type="black" width="100%" onClick={onWithdraw}>
-              Confirm
-            </Button>
-          )}
-
-          {isCompletedRef.current && (
-            <a onClick={() => closeModal()}>
-              <Button size="xl" type="black" width="100%">
-                Done
-              </Button>
-            </a>
-          )}
-        </Box>
+        )}
       </Box>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        motionPreset="slideInBottom"
+        blockScrollOnMount={true}
+      >
+        <ModalOverlay height="100vh" />
+        <ModalContent
+          borderRadius={{
+            sm: '20px 20px 0 0',
+            md: '20px',
+          }}
+          maxW={{
+            sm: '100vw',
+            md: '430px'
+          }}
+          marginTop={{
+            sm: `${marginHeight}px`,
+            md: 'calc(50vh - 125px)'
+          }}
+          height="468px"
+          overflow="auto"
+          mb="0"
+        >
+          <ModalCloseButton />
+          <ModalBody
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            width="100%"
+          >
+            <Box
+              background="#D9D9D9"
+              height="80px"
+              width="80px"
+              borderRadius="80px"
+              marginBottom="30px"
+            >
+              <Image src={OpIcon} />
+            </Box>
+            <Box fontSize="24px" fontWeight="700" marginBottom="14px">
+              Optimism network
+            </Box>
+            <Box
+              fontSize="16px"
+              textAlign="center"
+              marginBottom="40px"
+            >
+              Optimism is a Layer-2 scaling network for Ethereum that operates under a four-pillar design philosophy of simplicity, pragmatism, sustainability, and optimism.
+            </Box>
+            <Box width="100%">
+              <Button size="xl" type="blue" width="100%" onClick={onClose}>Got it</Button>
+            </Box>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
--- src/pages/send/index.tsx
+++ src/pages/send/index.tsx
@@ -1,13 +1,33 @@
 import { useState, useCallback } from 'react';
 import { Box } from '@chakra-ui/react';
-import SendForm from './SendForm';
+import SetAddress from './SetAddress';
+import SetAmount from './SetAmount';
+import Review from './Review';
 import FadeSwitch from '@/components/FadeSwitch';
 
 export default function Send({ isModal }: any) {
+  const [step, setStep] = useState(0);
+  const [withdrawAmount, setWithdrawAmount] = useState<any>('');
+  const [sendTo, setSendTo] = useState('');
+
+  const onPrev = useCallback(() => {
+    console.log('prev', step);
+    if (step > 0) {
+      setStep(prev => prev - 1);
+    }
+  }, [step]);
+
+  const onNext = useCallback(() => {
+    console.log('next');
+    setStep(step + 1);
+  }, [step]);
+
   return (
     <Box width="100%" height="100%">
-      <FadeSwitch key={0}>
-        <SendForm isModal={true} />
+      <FadeSwitch key={step}>
+        {step === 0 && <SetAddress isModal={true} onNext={onNext} />}
+        {step === 1 && <SetAmount isModal={true} withdrawAmount={withdrawAmount} setWithdrawAmount={setWithdrawAmount} sendTo={sendTo} setSendTo={setSendTo}  onPrev={onPrev} onNext={onNext} />}
+        {step === 2 && <Review isModal={true} onPrev={onPrev} />}
       </FadeSwitch>
     </Box>
   )
