5d338ccd2e215ffe45fc12cc1fb9863f7e6d9ad6
Terence Ge <terencegehui@yahoo.com>
2024-06-21 00:00:52 +0800

    chore: update

--- src/pages/recover/SetPasskey/index.tsx
+++ src/pages/recover/SetPasskey/index.tsx
@@ -4,7 +4,7 @@ import FadeId from '@/components/Icons/mobile/FaceId'
 
 export default function SetPasskey({ onNext, addingPasskey }: any) {
   return (
-    <Box width="100%" height="100%" padding="30px" paddingTop="138px" display="flex" alignItems="flex-start" justifyContent="center" flexDirection="column">
+    <Box width="100%" height="100%" padding="30px" paddingTop="117px" display="flex" alignItems="flex-start" justifyContent="center" flexDirection="column">
       <Box marginBottom="44px">
         <FadeId />
       </Box>
