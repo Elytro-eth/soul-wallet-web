38095100c61c4005d5b8aac5450a89e3e80f26d7
Terence Ge <terencegehui@yahoo.com>
2024-06-26 21:21:05 +0800

    chore: update setting

--- src/pages/settings/Guardian/Intro/index.tsx
+++ src/pages/settings/Guardian/Intro/index.tsx
@@ -21,23 +21,13 @@ export default function Intro({ onPrev, onNext }: any) {
   return (
     <Box width="100%" height="400px" padding="30px">
       <Box
-        width="120px"
-        height="120px"
-        borderRadius="120px"
-        margin="0 auto"
-        background="#F2F2F2"
-        opacity="0.55"
-      >
-        <Image src={GuardianIcon} />
-      </Box>
-      <Box
         width="100%"
         textAlign="center"
-        fontSize="24px"
-        fontWeight="700"
-        marginTop="18px"
+        fontSize="28px"
+        fontWeight="500"
+        color="#161F36"
       >
-        What’s guardian?
+        What’s recovery contact?
       </Box>
       <Box
         width="100%"
@@ -45,6 +35,7 @@ export default function Intro({ onPrev, onNext }: any) {
         fontSize="14px"
         fontWeight="400"
         marginTop="18px"
+        color="#676B75"
       >
         Recovery contacts are the wallet addresses from your own or trusted friends which will be requested for signature when your wallet is lost. Adding recovery contacts will protect your wallet from potential lose.
       </Box>
@@ -53,8 +44,8 @@ export default function Intro({ onPrev, onNext }: any) {
           <Box
             position="absolute"
             top="60px"
-            left="calc(50% - 122px)"
-            width="244px"
+            left="calc(-50vw + 32px)"
+            width="calc(100vw - 64px)"
           >
             <Menu
               isOpen={isOpen}
@@ -62,9 +53,9 @@ export default function Intro({ onPrev, onNext }: any) {
             >
               {() => (
                 <Box overflow="auto" ref={menuRef}>
-                  <MenuButton width="244px" as={Box} onClick={() => { isOpen ? onClose() : onOpen()}}>
+                  <MenuButton width="100%" as={Box} onClick={() => { isOpen ? onClose() : onOpen()}}>
                     <Box display="flex" alignItems="center" justifyContent="center">
-                      <Button  size="xl" type="blue" width="194px">Add recovery contact</Button>
+                      <Button  size="xl" type="gradientBlue" width="calc(100vw - 64px)" maxWidth="calc(430px - 64px)">Add recovery contact</Button>
                     </Box>
                   </MenuButton>
                   <MenuList
@@ -72,7 +63,8 @@ export default function Intro({ onPrev, onNext }: any) {
                     boxShadow="0px 4px 20px 0px rgba(0, 0, 0, 0.05)"
                   >
                     <MenuItem
-                      width="244px"
+                      width="calc(100vw - 64px)"
+                      maxWidth="calc(430px - 64px)"
                       position="relative"
                       padding="18px 27px"
                       borderBottom="1px solid #E4E4E4"
@@ -84,7 +76,8 @@ export default function Intro({ onPrev, onNext }: any) {
                       </Box>
                     </MenuItem>
                     <MenuItem
-                      width="100%"
+                      width="calc(100vw - 64px)"
+                      maxWidth="calc(430px - 64px)"
                       position="relative"
                       padding="18px 27px"
                       onClick={() => { openModal('addWalletGuardian'); onClose(); }}
