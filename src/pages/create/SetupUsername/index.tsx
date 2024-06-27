0a659190483f5a0fd6a89c598475d061074464b8
rocker <exrobbie@gmail.com>
2024-06-27 10:50:25 +0800

    chore: update error hint

--- src/pages/create/SetupUsername/index.tsx
+++ src/pages/create/SetupUsername/index.tsx
@@ -3,23 +3,22 @@ import Button from '@/components/mobile/Button';
 import InputLoading from '@/components/InputLoading';
 
 export default function SepupUsername({ value, onChange, onNext, checking, nameStatus }: any) {
-  const disabled = !value || nameStatus !== 0;
+  const valueIsNotString = typeof value !== 'string';
+  const lengthNotMatch = value.length < 3 || value.length > 24;
+  const valueHasSpecialChar = /[^a-zA-Z0-9_]/.test(value);
+
+  const disabled = !value || nameStatus !== 0 || valueIsNotString || lengthNotMatch || valueHasSpecialChar;
 
   return (
     <Box width="100%" padding="40px 30px" height="400px">
-      <Box
-        fontWeight="500"
-        fontSize="28px"
-        lineHeight="30px"
-        marginBottom="20px"
-      >
+      <Box fontWeight="500" fontSize="28px" lineHeight="30px" marginBottom="20px">
         Username
       </Box>
       <Box width="100%" marginBottom="30px">
         <Input
           value={value}
           autoFocus
-          onChange={e => onChange(e.target.value)}
+          onChange={(e) => onChange(e.target.value)}
           height="56px"
           spellCheck={false}
           fontSize="20px"
@@ -38,31 +37,22 @@ export default function SepupUsername({ value, onChange, onNext, checking, nameS
         <Box mt="1" h="44px" overflow={'hidden'}>
           {checking ? (
             <InputLoading />
+          ) : nameStatus === 0 ? (
+            <Box fontSize="14px" lineHeight="24px" fontWeight="600" minHeight="24px" color="#0CB700">
+              Coolio, you got a unique name
+            </Box>
+          ) : nameStatus === 1 ? (
+            <Box fontSize="14px" lineHeight="24px" fontWeight="700" minHeight="24px" color="#E83D26">
+              {valueIsNotString && 'Please enter a valid name'}
+              {lengthNotMatch && 'Name must be between 3 and 24 characters'}
+              {valueHasSpecialChar && 'Name must contain only letters, numbers, and underscores'}
+            </Box>
+          ) : nameStatus === 2 ? (
+            <Box fontSize="14px" lineHeight="24px" fontWeight="700" minHeight="24px" color="#E83D26">
+              This name has been taken. Please try a new one.
+            </Box>
           ) : (
-            <>
-              {nameStatus === 0 && (
-                <Box
-                  fontSize="14px"
-                  lineHeight="24px"
-                  fontWeight="600"
-                  minHeight="24px"
-                  color="#0CB700"
-                >
-                  Coolio, you got a unique name
-                </Box>
-              )}
-              {(nameStatus === 1 || nameStatus === 2) && (
-                <Box
-                  fontSize="14px"
-                  lineHeight="24px"
-                  fontWeight="700"
-                  minHeight="24px"
-                  color="#E83D26"
-                >
-                  {nameStatus === 1 ? 'Invalid username' : 'This name has been taken. Please try a new one.'}
-                </Box>
-              )}
-            </>
+            ''
           )}
         </Box>
       </Box>
