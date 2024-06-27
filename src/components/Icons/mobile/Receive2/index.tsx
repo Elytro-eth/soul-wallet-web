c949120d1658235b75ecc692c7234a19a0e634cf
Terence Ge <terencegehui@yahoo.com>
2024-06-27 18:37:09 +0800

    chore: fix

---
+++ src/components/Icons/mobile/Receive2/index.tsx
@@ -0,0 +1,10 @@
+const Receive = () => {
+  return (
+    <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fillRule="evenodd" clipRule="evenodd" d="M12.8665 13.1668C12.8665 13.535 12.568 13.8335 12.1998 13.8335H2.86646V4.50016C2.86646 4.13197 3.16493 3.8335 3.53312 3.8335C3.90131 3.8335 4.19979 4.13197 4.19979 4.50016V12.5002H12.1998C12.568 12.5002 12.8665 12.7986 12.8665 13.1668Z" fill="black" />
+      <path fillRule="evenodd" clipRule="evenodd" d="M13.3379 4.30501L6.67128 10.9717L5.72847 10.0289L12.3951 3.3622C12.6555 3.10186 13.0776 3.10186 13.3379 3.3622C13.5983 3.62255 13.5983 4.04466 13.3379 4.30501Z" fill="black" />
+    </svg>
+  );
+};
+
+export default Receive;
