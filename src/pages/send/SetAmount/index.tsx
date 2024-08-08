fb3c89b3da7acf6023a2a288c4ef4dc9c53eef5e
rocker <exrobbie@gmail.com>
2024-08-08 14:00:54 +0800

    chore: update balance format

--- src/pages/send/SetAmount/index.tsx
+++ src/pages/send/SetAmount/index.tsx
@@ -134,7 +134,7 @@ export default function SetAmount({
                     <Box fontSize="18px" color="#161F36" fontWeight="500">
                       {selectedToken.name}
                     </Box>
-                    <Box color="#95979C" fontSize="14px" fontWeight="400">Balance: {toFixed(selectedToken.tokenBalanceFormatted, 6)}</Box>
+                    <Box color="#95979C" fontSize="14px" fontWeight="400">Balance: {toFixed(selectedToken.tokenBalanceFormatted, 3)}</Box>
                   </Box>
                 </Box>
               )}
